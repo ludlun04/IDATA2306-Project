f7fbece86746070852d661fe61e6ca3bea532c07
stian oye <stianoyejenssen@gmail.com>
2025-01-16 16:42:45 +0100

    Initialized spring project

---
+++ src/main/java/no/ntnu/stud/idata2301_frontend/Idata2301FrontendApplication.java
@@ -0,0 +1,13 @@
+package no.ntnu.stud.idata2301_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Idata2301FrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Idata2301FrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/ntnu/stud/idata2301_frontend/Idata2301FrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.stud.idata2301_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Idata2301FrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
