ab54e6cb76ea7e95926f0eca483b46ba00c3a66d
stianoj <stianoj@ntnu.no>
2025-01-21 11:15:18 +0100

    Added anotations to the user entity

--- src/main/java/no/ntnu/stud/idata2306_project/model/User.java
+++ src/main/java/no/ntnu/stud/idata2306_project/model/User.java
@@ -1,12 +1,17 @@
 package no.ntnu.stud.idata2306_project.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class User{
   
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "user_id", nullable = false, updatable = false)
   Long id;
   String username;
   String password;
