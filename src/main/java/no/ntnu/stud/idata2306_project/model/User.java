6e344876c1681ebb8c79bb64216b439713bb783b
stianoyejenssen@gmail.com <stianoj@ntnu.no>
2025-01-21 11:23:48 +0100

    Added javadoc to the user entity

--- src/main/java/no/ntnu/stud/idata2306_project/model/User.java
+++ src/main/java/no/ntnu/stud/idata2306_project/model/User.java
@@ -6,6 +6,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+/**
+ * User entity class
+ * 
+ * <p> Will be used for possible authenticaion in the future
+ */
 @Entity
 public class User{
   
