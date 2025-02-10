26739164f62161db2b1aeed26ee4814618d95b89
Ludvik <ludvik.lund.hole@gmail.com>
2025-02-10 14:12:34 +0100

    added api doc for ocmpany, removed redundant dependency

--- src/main/java/no/ntnu/stud/idata2306_project/controller/CompanyController.java
+++ src/main/java/no/ntnu/stud/idata2306_project/controller/CompanyController.java
@@ -1,6 +1,7 @@
 package no.ntnu.stud.idata2306_project.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import no.ntnu.stud.idata2306_project.model.Company;
@@ -30,18 +31,34 @@ public class CompanyController {
     return companyRepository.findAll();
   }
 
+  @Operation(summary = "Get a company", description = "Get a company by id")
+  @Parameter(name = "id", description = "The id of the company to get", required = true)
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "Company that was found"),
+    @ApiResponse(responseCode = "404", description = "Company not found")
+  })
   @GetMapping("/{id}")
   public ResponseEntity<Company> getCompany(int id) {
     Company company = companyRepository.findById(id).orElse(null);
     return ResponseEntity.status(company == null ? HttpStatus.NOT_FOUND : HttpStatus.OK).body(company);
   }
 
+  @Operation(summary = "Add a company", description = "Add a new company")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "201", description = "Company that was added")
+  })
   @PostMapping()
   public ResponseEntity<Company> addCompany(Company company) {
     Company newCompany = companyRepository.save(company);
     return ResponseEntity.status(HttpStatus.CREATED).body(newCompany);
   }
 
+  @Operation(summary = "Delete a company", description = "Delete a company by id")
+  @Parameter(name = "id", description = "The id of the company to delete", required = true)
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "Company that was deleted"),
+    @ApiResponse(responseCode = "404", description = "Company not found")
+  })
   @DeleteMapping("/{id}")
   public ResponseEntity<Company> deleteCompany(int id) {
     Company company = companyRepository.findById(id).orElse(null);
