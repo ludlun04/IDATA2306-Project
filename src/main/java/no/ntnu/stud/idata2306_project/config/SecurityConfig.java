c64e605962c353c8341668f77077dd26d885e5d1
Kristian <garderkristian@gmail.com>
2025-02-24 11:15:43 +0100

    Implemented Auth Filter Chain

--- src/main/java/no/ntnu/stud/idata2306_project/config/SecurityConfig.java
+++ src/main/java/no/ntnu/stud/idata2306_project/config/SecurityConfig.java
@@ -1,9 +1,42 @@
 package no.ntnu.stud.idata2306_project.config;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
+/**
+ * Config class for security.
+ * Inspiration taken from:
+ * https://github.com/strazdinsg/app-dev/tree/main/security-demos/05-jwt-authentication
+ */
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
+  /**
+   * Configures the security filter chain.
+   *
+   * @param http the HttpSecurity object
+   * @throws Exception when security configuration fails
+   * @return the SecurityFilterChain object
+   */
+  @Bean
+  public SecurityFilterChain configureAuthorizationFilterChain(HttpSecurity http) throws Exception {
+    return http
+        .csrf(AbstractHttpConfigurer::disable)
+        .cors(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(auth -> auth.requestMatchers("/authenticate").permitAll())
+        .authorizeHttpRequests(auth -> auth.requestMatchers("/").permitAll())
+        .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .httpBasic(withDefaults()).build();
+  }
 }
