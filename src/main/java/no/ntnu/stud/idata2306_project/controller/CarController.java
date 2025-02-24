e547e03386c19e4d4ac8001e64c7542152340b9b
stian oye <stianoyejenssen@gmail.com>
2025-02-24 09:15:07 +0100

    Changed RequestParam annotation to PathVariable

--- src/main/java/no/ntnu/stud/idata2306_project/controller/CarController.java
+++ src/main/java/no/ntnu/stud/idata2306_project/controller/CarController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -51,7 +52,7 @@ public class CarController {
   }
 
   @DeleteMapping("/{id}")
-  public ResponseEntity<String> deleteCar(@RequestParam Long id) {
+  public ResponseEntity<String> deleteCar(@PathVariable Long id) {
       Optional<Car> car = carRepository.findById(id);
 
       if (car.isEmpty()) {
