9bdc53e5730f1731aca762daf3fde3ac5e755f58
stian oye <stianoyejenssen@gmail.com>
2025-02-24 09:04:09 +0100

    Implemented UserDetailsService and added findByUsername in UserRepository

--- src/main/java/no/ntnu/stud/idata2306_project/repository/UserRepository.java
+++ src/main/java/no/ntnu/stud/idata2306_project/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package no.ntnu.stud.idata2306_project.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import no.ntnu.stud.idata2306_project.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-  
+  public Optional<User> findByUsername(String username);
 }
---
+++ src/main/java/no/ntnu/stud/idata2306_project/service/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package no.ntnu.stud.idata2306_project.service;
+
+import java.util.Optional;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import no.ntnu.stud.idata2306_project.model.User;
+import no.ntnu.stud.idata2306_project.repository.UserRepository;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+  UserRepository userRepository;
+
+  public UserDetailsServiceImpl(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    Optional<User> user = userRepository.findByUsername(username);
+
+    if (user.isEmpty()) {
+      throw new UsernameNotFoundException("User not found");
+    }
+
+    return user.get();
+  }
+  
+}
