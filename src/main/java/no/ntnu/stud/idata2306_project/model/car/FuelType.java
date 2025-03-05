bb80e3e35406b7dd3e27318df2ccdce4c444ba0e
Ludvik Lund-Hole <ludvik.lund.hole@gmail.com>
2025-03-05 20:18:26 +0100

    added fuel type

---
+++ src/main/java/no/ntnu/stud/idata2306_project/model/car/FuelType.java
@@ -0,0 +1,63 @@
+package no.ntnu.stud.idata2306_project.model.car;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+/**
+ * Represents a fuel type.
+ */
+public class FuelType {
+
+  @Schema(description = "The id of the fuel type", example = "1")
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "fuel_id", nullable = false, updatable = false)
+  private long id;
+
+  @Schema(description = "The name of the fuel type", example = "Petrol")
+  @NotNull
+  @NotEmpty
+  @NotBlank
+  @Column(name = "fuel_name", nullable = false)
+  String name;
+
+  /**
+   * Creates a new fuel type.
+   */
+  public FuelType() {}
+
+  /**
+   * Creates a new fuel type.
+   *
+   * @param id the fuel type's id
+   * @param name the fuel type's name
+   */
+  public FuelType(Long id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  /**
+   * Returns the fuel type's id.
+   *
+   * @return the fuel type's id
+   */
+  public Long getId() {
+    return this.id;
+  }
+
+  /**
+   * Returns the fuel type's name.
+   *
+   * @return the fuel type's name
+   */
+  public String getName() {
+    return this.name;
+  }
+}
