23ff25326a5c1cf3662b14915a9e5a93022ec309
Ludvik Lund-Hole <ludvik.lund.hole@gmail.com>
2025-03-16 19:27:13 +0100

    created repository classes for car related entities

---
+++ src/main/java/no/ntnu/stud/idata2306_project/repository/CarBrandRepository.java
@@ -0,0 +1,8 @@
+package no.ntnu.stud.idata2306_project.repository;
+
+import no.ntnu.stud.idata2306_project.model.car.CarBrand;
+import org.springframework.data.repository.ListCrudRepository;
+
+public interface CarBrandRepository extends ListCrudRepository<CarBrand, Long> {
+
+}
