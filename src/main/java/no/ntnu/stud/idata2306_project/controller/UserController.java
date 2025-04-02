329fdc40684a1fededab7749abc3345b7521d037
stian oye <stianoyejenssen@gmail.com>
2025-04-02 12:14:20 +0200

    Added Request body annotation add user method

--- src/main/java/no/ntnu/stud/idata2306_project/controller/UserController.java
+++ src/main/java/no/ntnu/stud/idata2306_project/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -60,7 +61,7 @@ public class UserController {
       @ApiResponse(responseCode = "201", description = "User that was added")
   })
   @GetMapping("/add")
-  public ResponseEntity<User> addUser(User user) {
+  public ResponseEntity<User> addUser(@RequestBody User user) {
       User newUser = userRepository.save(user);
       return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
   }
