692ad42ad3de6a74e8f01563aee84a0edd0d7e66
stian oye <stianoyejenssen@gmail.com>
2025-04-08 14:10:38 +0200

    Added logging to userdetailsservice and authentication controller

--- src/main/java/no/ntnu/stud/idata2306_project/service/UserDetailsServiceImpl.java
+++ src/main/java/no/ntnu/stud/idata2306_project/service/UserDetailsServiceImpl.java
@@ -2,6 +2,8 @@ package no.ntnu.stud.idata2306_project.service;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -17,18 +19,23 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
   UserRepository userRepository;
 
+  Logger logger = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
+
   public UserDetailsServiceImpl(UserRepository userRepository) {
     this.userRepository = userRepository;
   }
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    logger.info("Loading user by username: {}", username);
     Optional<User> user = userRepository.findByUsername(username);
-
+    
     if (user.isEmpty()) {
+      logger.error("User not found: {}", username);
       throw new UsernameNotFoundException("User not found");
     }
 
+    logger.info("User found: {}", username);
     return new AccessUserDetails(user.get());
   }
 }
