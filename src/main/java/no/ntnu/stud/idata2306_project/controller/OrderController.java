5ea55dfd553bd2e053bbd5b1ee24d2d205c39bc1
Kristian <garderkristian@gmail.com>
2025-04-08 12:45:40 +0200

    OrderController Doc

--- src/main/java/no/ntnu/stud/idata2306_project/controller/OrderController.java
+++ src/main/java/no/ntnu/stud/idata2306_project/controller/OrderController.java
@@ -13,16 +13,34 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 
+/**
+ * Represents a controller for orders.
+ *
+ * <p>Contains the following endpoints:
+ * <ul>
+ *   <li>GET /order/history: Get all orders for the logged in user</li>
+ * </ul>
+ */
 @Tag(name = "Orders", description = "Endpoints for managing orders")
 @RestController
 @RequestMapping("/order")
 public class OrderController {
   private final OrderRepository orderRepository;
 
+  /**
+   * Creates a new OrderController.
+   *
+   * @param orderRepository the repository to use
+   */
   public OrderController(OrderRepository orderRepository) {
       this.orderRepository = orderRepository;
   }
 
+  /**
+   * Get all orders for the logged in user.
+   *
+   * @return a list of all orders for the logged in user
+   */
   @GetMapping("/history")
   public ResponseEntity<List<Order>> getCompanyOrders() {
     Authentication auth = SecurityContextHolder.getContext().getAuthentication();
