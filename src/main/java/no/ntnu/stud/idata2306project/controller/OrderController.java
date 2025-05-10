80283c6411017d1bf6e3dd7ab01d2932c806895f
Ludvik <ludvik.lund.hole@gmail.com>
2025-05-10 14:13:27 +0200

    javadoc, api doc, lint fix and checkstyle for order controller

--- src/main/java/no/ntnu/stud/idata2306project/controller/OrderController.java
+++ src/main/java/no/ntnu/stud/idata2306project/controller/OrderController.java
@@ -1,14 +1,13 @@
 package no.ntnu.stud.idata2306project.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import java.util.Optional;
-
 import no.ntnu.stud.idata2306project.dto.OrderRequestDto;
-import no.ntnu.stud.idata2306project.dto.OrderResponseDto;
 import no.ntnu.stud.idata2306project.exception.OrderNotFoundException;
 import no.ntnu.stud.idata2306project.model.car.Addon;
 import no.ntnu.stud.idata2306project.model.car.Car;
@@ -19,7 +18,6 @@ import no.ntnu.stud.idata2306project.security.AccessUserDetails;
 import no.ntnu.stud.idata2306project.service.CarService;
 import no.ntnu.stud.idata2306project.service.OrderService;
 import no.ntnu.stud.idata2306project.service.UserService;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -37,15 +35,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.v3.oas.annotations.tags.Tag;
 
 /**
  * Represents a controller for orders.
  *
- * <p>
- * Contains the following endpoints:
+ * <p>Contains the following endpoints:
  * <ul>
- * <li>GET /order/history: Get all orders for the logged in user</li>
+ * <li> Get all orders for the logged in user
+ * <li> Get all active orders for the logged in user
+ * <li> Add a new order
+ * <li> Get a order by its id
+ * <li> Remove an order by its id
+ * <li> Get all orders for a car
+ * <li> Get all orders for a company
  * </ul>
  */
 @Tag(name = "Orders", description = "Endpoints for managing orders")
@@ -56,12 +58,15 @@ public class OrderController {
   private final CarService carService;
   private final UserService userService;
   private final AddonRepository addonRepository;
-  private Logger logger = LoggerFactory.getLogger(OrderController.class);
+  private final Logger logger = LoggerFactory.getLogger(OrderController.class);
 
   /**
    * Creates a new OrderController.
    *
    * @param orderService the order service to use
+   * @param userService the user service to use
+   * @param carService the car service to use
+   * @param addonRepository the addon repository to use
    */
   public OrderController(OrderService orderService, UserService userService, CarService carService,
       AddonRepository addonRepository) {
@@ -72,13 +77,18 @@ public class OrderController {
   }
 
   /**
-   * Get all orders for the logged in user.
+   * Get all orders for the logged-in user.
    *
-   * @return a list of all orders for the logged in user
+   * @return a list of all orders for the logged-in user
    */
-  @Operation(summary = "Get all orders for the logged in user", description = "Get a list of all orders for the logged in user")
+  @Operation(
+      summary = "Get all orders for the logged in user",
+      description = "Get a list of all orders for the logged in user"
+  )
   @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "List of orders")
+      @ApiResponse(responseCode = "200", description = "List of orders"),
+      @ApiResponse(responseCode = "403", description = "Not authorized"),
+      @ApiResponse(responseCode = "400", description = "Invalid user information")
   })
   @GetMapping("/history")
   @PreAuthorize("hasAnyAuthority('USER', 'ADMIN')")
@@ -96,13 +106,18 @@ public class OrderController {
   }
 
   /**
-   * Get all active orders for the logged in user.
+   * Get all active orders for the logged-in user.
    *
-   * @return a list of all active orders for the logged in user
+   * @return a list of all active orders for the logged-in user
    */
-  @Operation(summary = "Get all active orders for the logged in user", description = "Get a list of all active orders for the logged in user")
+  @Operation(
+      summary = "Get all active orders for the logged in user",
+      description = "Get a list of all active orders for the logged in user"
+  )
   @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "List of active orders")
+      @ApiResponse(responseCode = "200", description = "List of active orders"),
+      @ApiResponse(responseCode = "403", description = "Not authorized"),
+      @ApiResponse(responseCode = "400", description = "Invalid user information")
   })
   @GetMapping("/active")
   @PreAuthorize("hasAnyAuthority('USER', 'ADMIN')")
@@ -122,17 +137,27 @@ public class OrderController {
   /**
    * Add a new order.
    *
-   * @param order the order to add
+   * @param accessUserDetails the user details of the logged-in user
+   * @param orderDto the order to add
    * @return the order that was added
    */
-  @Operation(summary = "Add a new order", description = "Add a new order")
+  @Operation(
+      summary = "Add a new order",
+      description = "Add a new order. Can only be done by a user."
+  )
   @ApiResponses(value = {
       @ApiResponse(responseCode = "201", description = "Order that was added"),
       @ApiResponse(responseCode = "400", description = "Invalid order"),
+      @ApiResponse(responseCode = "403", description = "Not authorized")
   })
   @PreAuthorize("hasAnyAuthority('USER')")
   @PostMapping("")
-  public ResponseEntity<Long> addOrder(@AuthenticationPrincipal AccessUserDetails accessUserDetails, @RequestBody OrderRequestDto orderDto) {
+  public ResponseEntity<Long> addOrder(
+      @Parameter(description = "User details of the logged-in user")
+      @AuthenticationPrincipal AccessUserDetails accessUserDetails,
+      @Parameter(description = "Order to add")
+      @RequestBody OrderRequestDto orderDto
+  ) {
     User user = this.userService.getUserById(accessUserDetails.getId());
     Optional<Car> optionalCar = this.carService.getCarById(orderDto.getCarId());
 
@@ -179,7 +204,7 @@ public class OrderController {
 
   /**
    * Get a order by its id.
-   * 
+   *
    * @param id the id of the order to get
    * @return the order with the given id
    */
@@ -192,8 +217,12 @@ public class OrderController {
   })
   @PreAuthorize("hasAnyAuthority('USER', 'ADMIN')")
   @GetMapping("/{id}")
-  public ResponseEntity<Order> getOrderById(@PathVariable Long id,
-      @AuthenticationPrincipal AccessUserDetails user) {
+  public ResponseEntity<Order> getOrderById(
+      @Parameter(description = "The id of the order to get")
+      @PathVariable Long id,
+      @Parameter(description = "The user details of the logged-in user")
+      @AuthenticationPrincipal AccessUserDetails user
+  ) {
     // Check if the id is valid
     if (id == null || id <= 0) {
       logger.error("Invalid id: {}", id);
@@ -215,7 +244,8 @@ public class OrderController {
     logger.info("User with id {} is admin: {}", user.getId(), isAdmin);
     boolean isOwnerOfOrder = order.getUser().getId().equals(user.getId());
     if (!isAdmin && !isOwnerOfOrder) {
-      logger.error("User with id {} is not authorized to access order with id {}", user.getId(), id);
+      logger.error("User with id {} is not authorized to access order with id {}",
+          user.getId(), id);
       return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 
@@ -238,7 +268,10 @@ public class OrderController {
   })
   @PreAuthorize("hasAnyAuthority('USER', 'ADMIN')")
   @DeleteMapping("/{id}")
-  public ResponseEntity<String> removeOrder(@PathVariable Long id,
+  public ResponseEntity<String> removeOrder(
+      @Parameter(description = "The id of the order to remove")
+      @PathVariable Long id,
+      @Parameter(description = "The user details of the logged-in user")
       @AuthenticationPrincipal AccessUserDetails user) {
     // Check if the id is valid
     if (id == null || id <= 0) {
@@ -272,15 +305,49 @@ public class OrderController {
     return ResponseEntity.ok("Order removed");
   }
 
+  /**
+   * Get all orders for a car.
+   *
+   * @param carId the id of the car to get orders for
+   * @return a list of orders for the car
+   */
+  @Operation(summary = "Get all orders for a car", description = "Get all orders for a car")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "List of orders for the car"),
+  })
   @GetMapping("car/{carId}")
-  public ResponseEntity<List<Order>> getOrdersByCarId(@PathVariable Long carId) {
+  public ResponseEntity<List<Order>> getOrdersByCarId(
+      @Parameter(description = "The id of the car to get orders for")
+      @PathVariable Long carId
+  ) {
     logger.info("Getting orders for car with id {}", carId);
     return ResponseEntity.ok(orderService.getOrdersByCarId(carId));
   }
 
+  /**
+   * Get all orders for a company. User must belong to company.
+   *
+   * @param companyId the id of the company to get orders for
+   * @param accessUserDetails the user details of the logged-in user
+   * @return a list of orders for the company
+   */
+  @Operation(
+      summary = "Get all orders for a company",
+      description = "Get all orders for a company."
+          + "Can only be accessed by users belonging to the company"
+  )
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "List of orders for the company"),
+      @ApiResponse(responseCode = "403", description = "Not authorized"),
+      @ApiResponse(responseCode = "404", description = "Company not found"),
+      @ApiResponse(responseCode = "500", description = "Unknown error")
+  })
   @GetMapping("company/{companyId}")
   @PreAuthorize("hasAnyAuthority('USER')")
-  public ResponseEntity<List<Order>> getOrdersByCompanyId(@PathVariable Long companyId, @AuthenticationPrincipal AccessUserDetails accessUserDetails) {
+  public ResponseEntity<List<Order>> getOrdersByCompanyId(
+      @PathVariable Long companyId,
+      @AuthenticationPrincipal AccessUserDetails accessUserDetails
+  ) {
     logger.info("Getting orders for company with id {}", companyId);
     User user = this.userService.getUserById(accessUserDetails.getId());
     try {
@@ -292,7 +359,8 @@ public class OrderController {
       logger.error("Company with id {} not found", companyId);
       return ResponseEntity.notFound().build();
     } catch (Exception e) {
-      logger.error("Unknown error encountered while getting orders for company with id {}. {}", companyId, e.getMessage());
+      logger.error("Unknown error encountered while getting orders for company with id {}. {}",
+          companyId, e.getMessage());
       return ResponseEntity.internalServerError().build();
     }
   }
