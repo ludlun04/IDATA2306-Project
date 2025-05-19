5b90c72f38d452c82122c9d36ee89026840331c1
Ludvik <ludvik.lund.hole@gmail.com>
2025-05-19 14:49:51 +0200

    dto doc

--- src/main/java/no/ntnu/stud/idata2306project/dto/CarFavoriteRequestDto.java
+++ src/main/java/no/ntnu/stud/idata2306project/dto/CarFavoriteRequestDto.java
@@ -1,26 +1,58 @@
 package no.ntnu.stud.idata2306project.dto;
 
+/**
+ * Represents a request to mark a car as favorite or not favorite.
+ *
+ * <p>This class is used to transfer data between the frontend and backend
+ * when a user wants to mark a car as favorite or not favorite.
+ */
 public class CarFavoriteRequestDto {
   private Long carId;
   private boolean isFavorite;
 
+  /**
+   * Creates a new instance of CarFavoriteRequestDto.
+   *
+   * @param carId the ID of the car
+   * @param isFavorite true if the car is marked as favorite, false otherwise
+   */
   public CarFavoriteRequestDto(Long carId, boolean isFavorite) {
     this.carId = carId;
     this.isFavorite = isFavorite;
   }
 
+  /**
+   * Sets the ID of the car.
+   *
+   * @return the ID of the car
+   */
   public Long getCarId() {
     return carId;
   }
 
+  /**
+   * Sets the ID of the car.
+   *
+   * @param carId the ID of the car
+   */
   public void setCarId(Long carId) {
     this.carId = carId;
   }
 
+  /**
+   * Checks if the car is marked as favorite.
+   *
+   * @return true if the car is marked as favorite, false otherwise
+   */
   public boolean isFavorite() {
     return isFavorite;
   }
 
+  /**
+   * Sets the favorite status of the car.
+   *
+   * @param isFavorite true if the car is marked as favorite, false otherwise
+   */
   public void setFavorite(boolean isFavorite) {
     this.isFavorite = isFavorite;
   }
--- src/main/java/no/ntnu/stud/idata2306project/dto/UserDto.java
+++ src/main/java/no/ntnu/stud/idata2306project/dto/UserDto.java
@@ -2,10 +2,14 @@ package no.ntnu.stud.idata2306project.dto;
 
 import java.util.Date;
 import java.util.List;
-
 import no.ntnu.stud.idata2306project.model.contact.Address;
 import no.ntnu.stud.idata2306project.model.contact.PhoneNumber;
 
+/**
+ * Represents a user data transfer object (DTO).
+ *
+ * <p>This class is used to transfer user data between the frontend and backend.
+ */
 public class UserDto {
   private String password;
   private String firstName;
@@ -16,27 +20,74 @@ public class UserDto {
   private Date dateOfBirth;
   private List<String> roles;
 
+  /**
+   * Gets the password of the user.
+   *
+   * @return the password of the user
+   */
   public String getPassword() {
     return password;
   }
+
+  /**
+   * Gets the first name of the user.
+   *
+   * @return the first name of the user
+   */
   public String getFirstName() {
     return firstName;
   }
+
+  /**
+   * Gets the last name of the user.
+   *
+   * @return the last name of the user
+   */
   public String getLastName() {
     return lastName;
   }
+
+  /**
+   * Gets the email of the user.
+   *
+   * @return the email of the user
+   */
   public String getEmail() {
     return email;
   }
+
+  /**
+   * Gets the phone number of the user.
+   *
+   * @return the phone number of the user
+   */
   public PhoneNumber getPhoneNumber() {
     return phoneNumber;
   }
+
+  /**
+   * Gets the address of the user.
+   *
+   * @return the address of the user
+   */
   public Address getAddress() {
     return address;
   }
+
+  /**
+   * Gets the date of birth of the user.
+   *
+   * @return the date of birth of the user
+   */
   public Date getDateOfBirth() {
     return dateOfBirth;
   }
+
+  /**
+   * Gets the roles of the user.
+   *
+   * @return the roles of the user
+   */
   public List<String> getRoles() {
     return roles;
   }
